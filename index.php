429f62b5eabfb59fe90ff6fefa1cdfa3e1be6a53
asinastretche <yochikaffiel@gmail.com>
2024-05-25 18:51:08 +0300

    выполнение первого задания,переход к выполнению второго

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
-echo '<br>' . '$arr = [1, 2, 3, 7, 31, 4, 1, 8, 6]';
+echo 'Задание 1. $arr = [1, 2, 3, 7, 31, 4, 1, 8, 6]';
 
 $arr = [1, 2, 3, 7, 31, 4, 1, 8, 6];
 $lengthArr = count($arr);
 
+$mixArr1 = array_slice($arr, 0,4);
+$mixArr2 = array_slice($arr, 4, 5);
+$resultMixArr = array_merge($mixArr2, $mixArr1);
+
 $sumArr = 0;
 
 echo '<br>' . "посчитать длину массива: $lengthArr";
-echo '<br>' . 'переместить первые 4 элемента массива в конец массива';
-echo '<br>' . 'получить сумму 4,5,6 элемента';
+echo '<br>' . "переместить первые 4 элемента массива в конец массива:"; print_r($resultMixArr) ;
+echo '<br>' . "получить сумму 4,5,6 элемента";
+
+echo "<br>"."<br>".'Задание 2. $firstArr = [
+  \'one\' => 1,
+  \'two\' => 2,
+  \'three\' => 3,
+  \'foure\' => 5,
+  \'five\' => 12
+]';
+echo "<br>".'$secondArr = [
+        \'one\' => 1,
+        \'seven\' => 22,
+        \'three\' => 32,
+        \'foure\' => 5,
+        \'five\' => 13,
+        \'six\' => 37
+   ]';
